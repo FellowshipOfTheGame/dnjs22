2d4ca88c8e3dc52b003f5a0305129b769734d455
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2018-10-07 02:21:20 -0300

    Initial message passing

---
+++ Assets/Scripts/Army.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Text;
+using UnityEngine.Networking.NetworkSystem;
+
+public class Army : MonoBehaviour
+{
+    public class MyMsgType
+    {
+        public static short Hello = MsgType.Highest + 1;
+    };
+
+    public class HelloMessage : MessageBase
+    {
+        public string hello;
+    }
+
+    NetworkClient myClient;
+    //With the @ before the string, we can split a long string in many lines without getting errors
+    private string json = @"{
+		'hello':'world', 
+		'foo':'bar', 
+		'count':25
+	}";
+
+    void Start()
+    {
+        SetupClient();
+    }
+
+    public void SetupClient()
+    {
+        myClient = new NetworkClient();
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);
+        myClient.Connect("127.0.0.1", 4444);
+    }
+
+    public void SetupLocalClient()
+    {
+        myClient = ClientScene.ConnectLocalServer();
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected");
+        HelloMessage msg = new HelloMessage();
+        msg.hello = json;
+        myClient.Send(MyMsgType.Hello, msg);
+    }
+}
---
+++ Assets/Scripts/Commander.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class Commander : NetworkBehaviour
+{
+    public class MyMsgType
+    {
+        public static short Hello = MsgType.Highest + 1;
+    };
+
+    void Start()
+    {
+        SetupServer();
+    }
+
+    public void SetupServer()
+    {
+        NetworkServer.Listen(4444);
+        NetworkServer.RegisterHandler(MyMsgType.Hello, OnServerReadyToBeginMessage);
+        Debug.Log("Created server");
+    }
+
+    void OnServerReadyToBeginMessage(NetworkMessage netMsg)
+    {
+        var beginMessage = netMsg.ReadMessage<StringMessage>();
+        Debug.Log("received OnServerReadyToBeginMessage " + beginMessage.value);
+    }
+}
