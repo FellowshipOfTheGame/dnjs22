e0e6c21d8bb2353301c2088136f9f1a704bf5df2
Fabricio <fabricio-gf@hotmail.com>
2018-10-20 00:02:55 -0300

    Fixed map scroll

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour {
+
+	[SerializeField] private float PanSpeed = 20f;
+
+	[SerializeField] private float[] BoundsX = new float[]{-10f,5f};
+
+	private Camera cam;
+	private Vector3 lastPanPosition;
+	private int panFingerId; // Touch mode only
+
+	private bool wasZoomingLastFrame; // Touch mode only
+	private Vector2[] lastZoomPositions; // Touch mode only
+	private bool isMenuOpen = false;
+	private bool isDragging = false;
+
+	void Awake() {
+		cam = Camera.main;
+	}
+
+	void Update() {
+		if (isMenuOpen || !isDragging) {
+			return;
+		}
+		if (Input.touchSupported && Application.platform != RuntimePlatform.WebGLPlayer) {
+			HandleTouch();
+		} else {
+			HandleMouse();
+		}
+	}
+
+	public void StartDragging(){
+		print("start");
+		isDragging = true;
+	}
+
+	public void StopDragging(){
+		print("stop");
+		isDragging = false;
+	}
+
+	void HandleTouch() {
+		if(Input.touchCount == 1){
+			// If the touch began, capture its position and its finger ID.
+			// Otherwise, if the finger ID of the touch doesn't match, skip it.
+			Touch touch = Input.GetTouch(0);
+			if (touch.phase == TouchPhase.Began) {
+				lastPanPosition = touch.position;
+				panFingerId = touch.fingerId;
+			} else if (touch.fingerId == panFingerId && touch.phase == TouchPhase.Moved) {
+				PanCamera(touch.position);
+			}
+		}
+	}
+
+	void HandleMouse() {
+		// On mouse down, capture it's position.
+		// Otherwise, if the mouse is still down, pan the camera.
+		if (Input.GetMouseButtonDown(0)) {
+			lastPanPosition = Input.mousePosition;
+		} else if (Input.GetMouseButton(0)) {
+			PanCamera(Input.mousePosition);
+		}
+	}
+
+	void PanCamera(Vector3 newPanPosition) {
+		// Determine how much to move the camera
+		Vector3 offset = cam.ScreenToViewportPoint(lastPanPosition - newPanPosition);
+		Vector3 move = new Vector3(-offset.x * PanSpeed, 0, 0);
+		
+		// Perform the movement
+		transform.Translate(move, Space.World);  
+		
+		// Ensure the camera remains within bounds.
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp(transform.position.x, BoundsX[0], BoundsX[1]);
+		transform.position = pos;
+
+		// Cache the position
+		lastPanPosition = newPanPosition;
+	}
+}
--- Assets/Scripts/MapActions.cs
+++ Assets/Scripts/MapActions.cs
@@ -1,62 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MapActions : MonoBehaviour {
 
-	[SerializeField] private GameObject MovableArea;
-	private Transform MovableTransform;
-	private Vector3 StartDragPosition;
-	private Vector3 CurrentDragPosition;
-	private bool isDragging;
+	public GameObject selectedBase = null;
+	public GameObject troopsWindow;
+	public UIActions MenuManager;
 
 	void Start(){
-		MovableTransform = MovableArea.transform;
+
 	}
 
 	
 	public void TouchMap(){
-		print("TOUCH!");
-		StartDragPosition = Input.mousePosition;
-		isDragging = true;
+
 	}
 
 	public void DragMap(){
 
-#if UNITY_ANDROID
-		//usando controle de toque no celular
-		if(isDragging && Input.touchCount == 0){
-			isDragging = false;
-		}
-		else if(isDragging && Input.touchCount == 1){
-			CurrentDragPosition = Input.GetTouch(0).position;
-			var DragOffset = Camera.main.ScreenToWorldPoint(CurrentDragPosition.x) - Camera.main.ScreenToWorldPoint(StartDragPosition.x); 
-			MovableTransform.position = new Vector3(MovableTransform.position.x + DragOffset, MovableTransform.position.y, MovableTransform.position.z);
-		}
-#endif
-
-#if UNITY_EDITOR
-		//usando o mouse
-		if(isDragging){
-			CurrentDragPosition = Input.mousePosition;
-			var DragOffset = CurrentDragPosition.x - StartDragPosition.x;
-			MovableTransform.position = new Vector3(MovableTransform.position.x + DragOffset, MovableTransform.position.y, MovableTransform.position.z);
-	
-		}
-#endif
 	}
 
 	public void BeginDragMap(){
-		print("entrou");
-		if(Input.touchCount == 1){
-			StartDragPosition = Input.GetTouch(0).position;
-			isDragging = true;
-		}
 
-#if UNITY_EDITOR
-		StartDragPosition = Input.mousePosition;
-		isDragging = true;
-#endif
+	}
 
+	public void ConfirmSelectBase(GameObject b){
+		// if(selectedBase != null && selectedBase == b){
+		// 	MenuManager.ToggleWindow(troopsWindow);
+		// }
 	}
 }
