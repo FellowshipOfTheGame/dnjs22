9945abd171688a1581917fa6597e9d007c0e501c
Fabricio <fabricio-gf@hotmail.com>
2018-10-31 15:59:17 -0300

    Added sprites to game

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour {
+
+	public Transform SpinningIcon;
+	// Update is called once per frame
+	void Update () {
+		SpinningIcon.Rotate(0, 60*Time.deltaTime, 0);
+	}
+}
