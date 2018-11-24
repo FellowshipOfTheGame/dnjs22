68afc6d5b60abac3a366205ae48bbb7d9ce203e2
GabrielBCarvalho <gabrielmattheus.carvalho@gmail.com>
2018-11-24 19:19:40 -0200

    client

--- Assets/Scripts/ArmyBehaviour.cs
+++ Assets/Scripts/ArmyBehaviour.cs
@@ -9,60 +9,96 @@ using Newtonsoft.Json;
 
 public class ArmyBehaviour : MonoBehaviour
 {
-    NetworkClient myClient;
+	NetworkClient myClient;
 
-    [SerializeField] private UIActions MenuManager;
-    [SerializeField] private string UniqueID;
-    [SerializeField] private Team MyTeam;
+	[SerializeField] private UIActions MenuManager;
+	[SerializeField] private string UniqueID;
+	[SerializeField] private Team MyTeam;
 
-    void Start()
-    {
-        SetupClient();
-    }
+	[SerializeField] private int team;
+	[SerializeField] private int money;
+	[SerializeField] private DateTime lastLogin;
 
-    public void SetupClient()
-    {
-        myClient = new NetworkClient();
-        myClient.RegisterHandler(MsgType.Connect, OnConnected);
-        // myClient.RegisterHandler(MyMsgType.MessageCommand, PrintReturn);
-        myClient.Connect("lessertest.eastus.cloudapp.azure.com", 4444);
-        UniqueID = SystemInfo.deviceUniqueIdentifier;
-    }
+	void Start()
+	{
+		SetupClient();
+	}
 
-    public void SetupLocalClient()
-    {
-        myClient = ClientScene.ConnectLocalServer();
-        myClient.RegisterHandler(MsgType.Connect, OnConnected);
-    }
+	public void SetupClient()
+	{
+		myClient = new NetworkClient();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+		myClient.RegisterHandler (MyMsgType.MessageCommand, OnCommandReturn);
+		// myClient.RegisterHandler(MyMsgType.MessageCommand, PrintReturn);
+		//myClient.Connect("lessertest.eastus.cloudapp.azure.com", 4444);
+		myClient.Connect("127.0.0.1", 4444);
+		UniqueID = SystemInfo.deviceUniqueIdentifier;
+	}
 
-    // void PrintReturn(NetworkMessage netMsg)
-    // {
-    //     string beginMessage = netMsg.ReadMessage<StringMessage>().value;
-    //     MessageCommand msg = JsonConvert.DeserializeObject<MessageCommand>(beginMessage);
-    //     Debug.Log("Client\nIssue: " + msg.issue + "\nPlayer Id: " + msg.player + "\nTeam name: " + msg.troop.team.name);
-    // }
+	public void SetupLocalClient()
+	{
+		myClient = ClientScene.ConnectLocalServer();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+	}
 
-    public void OnConnected(NetworkMessage netMsg)
-    {
-        Debug.Log("Connected");
-        MenuManager.ToggleLoadingScreen();
-    }
+	private void OnCommandReturn(NetworkMessage netMsg){
+		string beginMessage = netMsg.ReadMessage<StringMessage>().value;
 
-    public void BuyTroop(){
-        print("BOUGHT 1 TROOP");
-        MessageCommand msg = new MessageCommand();
-        msg.issue = DateTime.Now;
-        msg.player = UniqueID;
-        msg.troop = new Troop(1, MyTeam);
-        myClient.Send(MyMsgType.MessageCommand, new StringMessage(JsonConvert.SerializeObject(msg)));
-    }
+		if(beginMessage != "Error"){
+			MessageCommand msg = JsonConvert.DeserializeObject<MessageCommand>(beginMessage);
+			if(msg.type == 2){	// Login
+				team = msg.team;
+				money = msg.money;
+				lastLogin = Convert.ToDateTime (msg.lastLogin);
+				Debug.Log("Login successfull!");
+			}
+		}
+		else
+			Debug.Log("Error found at receiving message");
+	}
+	// void PrintReturn(NetworkMessage netMsg)
+	// {
+	//     string beginMessage = netMsg.ReadMessage<StringMessage>().value;
+	//     MessageCommand msg = JsonConvert.DeserializeObject<MessageCommand>(beginMessage);
+	//     Debug.Log("Client\nIssue: " + msg.issue + "\nPlayer Id: " + msg.player + "\nTeam name: " + msg.troop.team.name);
+	// }
 
-    public void SendTroop(int troopNum){
-        print("SENT " + troopNum + " TROOPS");
-        MessageCommand msg = new MessageCommand();
-        msg.issue = DateTime.Now;
-        msg.player = UniqueID;
-        msg.troop = new Troop(troopNum, MyTeam);
-        myClient.Send(MyMsgType.MessageCommand, new StringMessage(JsonConvert.SerializeObject(msg)));
-    }
+	public void OnLoginReturn(NetworkMessage netMsg){
+		string beginMessage = netMsg.ReadMessage<StringMessage>().value;
+		MessageCommand msg = JsonConvert.DeserializeObject<MessageCommand>(beginMessage);
+	}
+
+	private void OnConnected(NetworkMessage netMsg)
+	{
+		Debug.Log("Connected");
+		MenuManager.ToggleLoadingScreen();
+
+        Login();
+	}
+
+	public void BuyTroop(){
+		print("BOUGHT 1 TROOP");
+		MessageCommand msg = new MessageCommand();
+		msg.issue = DateTime.Now;
+		msg.player = UniqueID;
+		msg.troop = new Troop(1, MyTeam);
+		myClient.Send(MyMsgType.MessageCommand, new StringMessage(JsonConvert.SerializeObject(msg)));
+	}
+
+	public void SendTroop(int troopNum){
+		print("SENT " + troopNum + " TROOPS");
+		MessageCommand msg = new MessageCommand();
+		msg.issue = DateTime.Now;
+		msg.player = UniqueID;
+		msg.troop = new Troop(troopNum, MyTeam);
+		myClient.Send(MyMsgType.MessageCommand, new StringMessage(JsonConvert.SerializeObject(msg)));
+	}
+
+	private void Login(){
+		MessageCommand msg = new MessageCommand();
+		msg.player = UniqueID;
+        msg.user = UniqueID;
+		msg.type = 2;
+		myClient.Send (MyMsgType.MessageCommand, new StringMessage (JsonConvert.SerializeObject (msg)));
+	}
 }
--- Assets/Scripts/utils.cs
+++ Assets/Scripts/utils.cs
@@ -5,22 +5,41 @@ using UnityEngine.Networking;
 public static class MyMsgType
 {
     public static short MessageCommand = MsgType.Highest + 1;
-    public static short CommandAddedSuccesfull = MsgType.Highest + 1;
+    public static short CommandAddedSuccesfull = MsgType.Highest + 2;
+    public static short BoughtSuccesfull = MsgType.Highest + 3;
+    public static short AddedSuccesfull = MsgType.Highest + 4;
+
+	public static short Login = MsgType.Highest + 5;
+	public static short RegisterSuccessfull = MsgType.Highest + 6;
 };
 
 public class MessageCommand
 {
+    public int type;
     public DateTime issue;
     public Troop troop;
+    public int buy;
     public string player;
     public int target;
+
+	public string user;
+    public int id;
+	public string password = "42";
+	public int team;
+	public string lastLogin;
+	public int money;
 }
 
 public class Command
 {
     public DateTime issue;
-    public double cost;
     public Troop troop;
+    public int cost;
     public string player;
     public int target;
+
+	//public int
+	public string user;
+	public string password = "42";
+	public NetworkMessage netMsg;
 }
